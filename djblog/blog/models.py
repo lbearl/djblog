42f0c85a524e396e86e3204d3bb868f662f03a2e
Luke Bearl <bearl@artemistechnologies.com>
2012-02-12 18:44:09 -0600

    Fixing my stupid

---
+++ djblog/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(User)
+    blog_entry = models.TextField()
+
+class comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comment = models.TextField()
+    moderated = models.BooleanField()
+
