c8ff93057463977028c8a8fd4a89ad12826122ff
Luke Bearl <bearl@artemistechnologies.com>
2012-02-21 20:04:06 -0600

    And now I have learned the dangers of copying code from another project...

--- djblog/api/urls.py
+++ djblog/api/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 from djblog.api.handlers import BlogHandler, CommentHandler
 
-product_handler = Resource(ProductHandler)
-registry_handler = Resource(RegistryHandler)
+blog_handler = Resource(BlogHandler)
+comment_handler = Resource(CommentHandler)
 
 urlpatterns = patterns('',
     #url(r'^product/(?P<id>[^/]+)/', identifier_handler),
-    url(r'^blog/', product_handler),
-    url(r'^comment/', registry_handler),
+    url(r'^blog/', blog_handler),
+    url(r'^comment/', comment_handler),
     )
