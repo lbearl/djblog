5ab362dbf0f274199cae4041af0583f75f18c095
Luke Bearl <bearl@artemistechnologies.com>
2012-02-21 19:45:29 -0600

    Committing piston logic

---
+++ djblog/api/handlers.py
@@ -0,0 +1,39 @@
+from piston.handler import BaseHandler
+from djblog.blog import Blog, Comment
+from django.contrib.auth.models import User
+
+class BlogHandler(BaseHandler):
+    """
+    This will allow for CRUD operations on blog entries
+    """
+    allowed_methods = ('GET', 'PUT', 'POST', 'DELETE')
+    model = Blog
+    fields = ('blog_title', 'author', 'blog_summary', 'blog_entry')
+    
+    def read(self, request, blog_title=None):
+        base = Blog.objects
+        return base.all()
+
+class CommentHandler(BaseHandler):
+    """
+    This will allow for CRUD operations on blog comments
+    """
+    allowed_methods = ('GET', 'PUT', 'POST')
+    model = Comment
+    fields = ('name', 'email', 'comment', 'moderated')
+
+    def read(self, request, comment=None):
+        base = Comment.objects
+        return base.all()
+
+class BlogAndCommentHandler(BaseHandler):
+    """
+    This will retrieve a Blog and ALL comments
+    """
+    allowed_methods = ('GET')
+    model = Blog
+    fields = ('blog_name', 'author', 'blog_summary', 'blog_entry', ('comment', ()))
+
+    def read(self, request, blog_title=None):
+        base = Blog.objects
+        return base.all()
---
+++ djblog/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from piston.resource import Resource
+from blisst.api.handlers import ProductHandler, RegistryHandler
+
+product_handler = Resource(ProductHandler)
+registry_handler = Resource(RegistryHandler)
+
+urlpatterns = patterns('',
+    #url(r'^product/(?P<id>[^/]+)/', identifier_handler),
+    url(r'^blog/', product_handler),
+    url(r'^comment/', registry_handler),
+    )
